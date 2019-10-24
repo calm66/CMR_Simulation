11a9c8a163ecc4f48b48f9ef29a7479e1481a50f
Chao Yao <chao.yao@tu-dresden.de>
2019-10-24 11:10:45 +0200

    add solutions for exercise 1, move both models from \exercise to \blocks.

--- exercises/exp_ddrive_test.m
+++ exercises/exp_ddrive_test.m
@@ -13,15 +13,28 @@ function exp = exp_ddrive_test(varargin)
     
     % Instantiate the robot
     exp.robot.platform = model_platform2d_ddrive([2 0.75 90*pi/180]);
-    %exp.robot.platform.wheelRadius = [0.030 0.0303];    
-    %exp.robot.platform.wheelDistance = 0.255;
     exp.robot.radius = 0.14;
     exp.robot.color = [0 0 1];
-    exp.robot.wheelRadius = 0.03;
-    exp.robot.wheelDistance = 0.25;   
+    
+    % Add modeling error
+    useRadiusError = false;
+    useDistanceError = false;
+    if useRadiusError
+        exp.robot.platform.wheelRadius = [0.030 0.0303]; 
+    else
+        exp.robot.wheelRadius = 0.03;
+    end   
+    if useDistanceError
+        exp.robot.platform.wheelDistance = 0.255; 
+    else
+        exp.robot.wheelDistance = 0.25;
+    end 
     
     % Add rangefinder
-%     exp.robot.sensor = sensor_rangefinder2d();
+    useRangfinder = false;
+    if useRangfinder
+        exp.robot.sensor = sensor_rangefinder2d();
+    end
     
     % add a sink to convince the simulator engine, that the robot is used.
     % Otherwise the engine would 'optimize' the robot away
@@ -42,25 +55,29 @@ function exp = exp_ddrive_test(varargin)
         % Otherwise, use a controller that make the robot move on a square 
         % path
         exp.robot.controller = guidance_ddrive_square();
-        exp.robot.controller.length = 4;
+        exp.robot.controller.length = 2;
     end
     
-    
-%     exp.robot2.platform = model_platform2d_omni([2.5 3 90*pi/180]);
-%     exp.robot2.radius = 0.14;
-%     exp.robot2.color = [0.5 0 1]; % Farbe setzen
-%     exp.robot2.sink = block_sink('platform');
-%     exp.robot2.controller = guidance_omni_circle();
-%     exp.robot2.controller.radius = 1.5;
-%     exp.robot2.controller.rotVelocity = 20*pi/180;
+    addRobot2 = true;
+    if addRobot2
+        exp.robot2.platform = model_platform2d_omni([2.5 3 90*pi/180]);
+        exp.robot2.radius = 0.14;
+        exp.robot2.color = [0.5 0 1]; % Farbe setzen
+        exp.robot2.sink = block_sink('platform');
+        exp.robot2.controller = guidance_omni_circle();
+        exp.robot2.controller.radius = 1.5;
+        exp.robot2.controller.rotVelocity = 20*pi/180;
+    end
     
     % The 'depends' field controls, which blocks should not be optimized
     % away, even if their outputs are unused.
     % (Note: This mechanism does not work with continuous blocks like the 
     % robot. Therefore we had to add the sink above, which is discrete and
     % can be put into the depends list)
-	exp.depends = {'*sink', '*environment*'};
-%     exp.depends{end + 1} ='*sensor'; 
+	  exp.depends = {'*sink', '*environment*'};
+    if useRangfinder
+        exp.depends{end + 1} ='*sensor';
+    end
     
     % Some parameters that influence the appereance of the display area
     exp.display.settings = {'XGrid', 'on', 'YGrid', 'on', 'Layer', 'top', 'XLim', [0 8], 'YLim', [0 6]};
